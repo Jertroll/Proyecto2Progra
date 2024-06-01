fe8914b21a8d0e6f805d73b5c2fa3bb11198b2a0
Jertroll <jeremyblanco20@gmail.com>
2024-05-31 20:19:27 -0600

    Avance 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home/home.component';
+import { LoginComponent } from './components/login/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+{path: '', component:HomeComponent}
+{path: 'home', component:HomeComponent},
+{path: 'login', component:LoginComponent}
+
+];

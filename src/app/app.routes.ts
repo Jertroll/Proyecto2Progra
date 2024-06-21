4c73931e1cf57a6a178f94bbdd6c66ec9a65024d
Kristhel04 <149755958+Kristhel04@users.noreply.github.com>
2024-06-21 00:13:28 -0600

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,22 @@
+
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home/home.component';
 import { LoginComponent } from './components/login/login/login.component';
 import { ProduagregarComponent } from './components/produagregar/produagregar.component';
 import { PrudutosComponent } from './components/prudutos/prudutos.component';
 import { ComprasComponent } from './components/compras/compras.component';
+import { UserAgregarComponent } from './components/UserCreate/user-agregar/user-agregar.component';
 
 export const routes: Routes = [
+{path: '', redirectTo: '/home', pathMatch: 'full' },
 {path: '', component:HomeComponent},
 {path: 'home', component:HomeComponent},
 {path: 'login', component:LoginComponent},
 {path: 'produagregar', component:ProduagregarComponent},
 {path: 'prudutos', component:PrudutosComponent},
-{path: 'compras', component:ComprasComponent}
+{path: 'compras', component:ComprasComponent},
+{path: "userRUD", component:UserAgregarComponent},
+{path: 'user-agregar', component:UserAgregarComponent},
+{ path: 'user/:id', component: UserAgregarComponent },
+{ path: '**', redirectTo: '/home' }
 ];
---
+++ src/app/components/UserCreate/user-agregar/user-agregar.component.ts
@@ -0,0 +1,72 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { User } from '../../../models/User';
+import { timer } from 'rxjs';
+import { UserService } from '../../../services/user.component';
+import { RouterModule } from '@angular/router';
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule, RouterModule,CommonModule],
+  templateUrl: './user-agregar.component.html',
+  styleUrl: './user-agregar.component.css'
+})
+export class UserAgregarComponent {
+  public user:User;
+  public status:number;
+  public fileName:string;
+  constructor(
+    private UserService:UserService
+  ){
+    this.status=-1;
+    this.user=new User(0,"","",1,"",1,"","","","")
+    this.fileName="";
+  }
+
+  onSubmit(form:any){
+    this.user.rol='user';
+    this.UserService.create(this.user).subscribe({
+      next:(response)=>{
+        console.log(response);
+        if(response.status==201){
+          form.reset();            
+          this.changeStatus(0);
+        }else{
+          this.changeStatus(1);
+        }
+      },
+      error:(error:Error)=>{
+        this.changeStatus(2);
+      }
+    })
+  }
+  uploadImage(event:any){
+    const file: File = event.target.files[0];
+    if (file) {
+      this.fileName = file.name;
+      const formData = new FormData();
+      formData.append('file0', file);
+
+      this.UserService.uploadImage(formData).subscribe({
+        next: (response: any) => {
+          console.log(response);
+          if (response.status === 201) {
+            this.user.imagen = response.filename;
+          }
+        },
+        error: (err: any) => {
+          console.log(err);
+        }
+      });
+    }
+  }
+
+  changeStatus(st:number){
+    this.status=st;
+    let countdown=timer(5000);
+    countdown.subscribe(n=>{
+      this.status=-1;
+    })
+  }
+}

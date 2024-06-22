89e7a4caef4bd8246c5902052f9bdb7193453685
Jertroll <jeremyblanco20@gmail.com>
2024-06-22 17:33:01 -0600

    listt-factura

---
+++ src/app/components/factura-list-user/factura-list-user.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-factura-list-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './factura-list-user.component.html',
+  styleUrl: './factura-list-user.component.css'
+})
+export class FacturaListUserComponent {
+
+}
